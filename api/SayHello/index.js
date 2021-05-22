5ada0bbb9a2b736925a9db5b6eb4740e51980f51
Andrew Burgess <ajburgess>
2021-05-22 08:30:12 +0100

    colour changes

--- api/SayHello/index.js
+++ api/SayHello/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name) || process.env.DefaultName);
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "HELLO, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
